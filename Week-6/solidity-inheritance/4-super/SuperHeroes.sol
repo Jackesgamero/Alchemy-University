8adad7fd4f961abbad0793ffe531e7f64fe1b5f0
Jaime Martinez Gamero <71594504+Jackesgamero@users.noreply.github.com>
2025-12-12 20:27:03 +0100

    solidity-inheritance

---
+++ Week-6/solidity-inheritance/4-super/SuperHeroes.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "./Hero.sol";
+
+// TODO: create Mage/Warrior Heroes
+
+contract Mage is Hero(50){
+
+    function attack(address enemy) public override{
+        Enemy e = Enemy(enemy);
+        e.takeAttack(AttackTypes.Spell);
+        super.attack(enemy);
+    }
+}
+
+contract Warrior is Hero(200){
+
+    function attack(address enemy) public override{
+        Enemy e = Enemy(enemy);
+        e.takeAttack(AttackTypes.Brawl);
+        super.attack(enemy);
+    }    
+}
